61a0e88dd8e477f51d4bcdefb9e1483c9d1dc5dd
Angel Velasquez <pcisavel@upc.edu.pe>
2025-06-04 13:12:48 -0500

    feat(tutorials): add category entity.

---
+++ ACME.LearningCenterPlatform.API/Publishing/Domain/Model/Entities/Category.cs
@@ -0,0 +1,29 @@
+namespace ACME.LearningCenterPlatform.API.Publishing.Domain.Model.Entities;
+
+/// <summary>
+/// Represents a category in the publishing context.
+/// </summary>
+public class Category
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+
+    /// <summary>
+    /// Default constructor for Category.
+    /// </summary>
+    public Category()
+    {
+        Name = string.Empty;
+    }
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="Category"/> class with a specified name.
+    /// </summary>
+    /// <param name="name">
+    /// The name of the category.
+    /// </param>
+    public Category(string name)
+    {
+        Name = name;
+    }
+}
