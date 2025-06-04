dc10d2dfba1b4879ed1a1671cc9a03ef801d916e
Angel Velasquez <pcisavel@upc.edu.pe>
2025-06-04 12:42:04 -0500

    feat(publishing): add profile id value object.

---
+++ ACME.LearningCenterPlatform.API/Publishing/Domain/Model/ValueObjects/ProfileId.cs
@@ -0,0 +1,7 @@
+namespace ACME.LearningCenterPlatform.API.Publishing.Domain.Model.ValueObjects;
+
+/// <summary>
+/// Represents the identifier for a profile.
+/// </summary>
+/// <param name="Id"></param>
+public record ProfileId(int Id);
