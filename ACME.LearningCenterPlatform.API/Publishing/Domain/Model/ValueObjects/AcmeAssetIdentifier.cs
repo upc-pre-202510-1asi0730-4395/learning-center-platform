efb8c80ed1eaf22880e6c0b98cd1c3fc9bea79fb
Angel Velasquez <pcisavel@upc.edu.pe>
2025-06-11 13:05:51 -0500

    feat(publishing): modify default asset identifier value.

--- ACME.LearningCenterPlatform.API/Publishing/Domain/Model/ValueObjects/AcmeAssetIdentifier.cs
+++ ACME.LearningCenterPlatform.API/Publishing/Domain/Model/ValueObjects/AcmeAssetIdentifier.cs
@@ -11,5 +11,5 @@ public record AcmeAssetIdentifier(Guid Identifier)
     /// <summary>
     /// Initializes a new instance of the <see cref="AcmeAssetIdentifier"/> record with a specified identifier.
     /// </summary>
-    public AcmeAssetIdentifier() : this(Guid.Empty) { }
+    public AcmeAssetIdentifier() : this(Guid.NewGuid()) { }
 };
