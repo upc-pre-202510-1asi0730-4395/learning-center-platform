b3692d28d21e01b8de412611f52c9162b4ecf8c9
Angel Velasquez <pcisavel@upc.edu.pe>
2025-06-17 22:56:06 -0500

    feat(publishing): publish category created event when creation is completed.

--- ACME.LearningCenterPlatform.API/Publishing/Application/Internal/CommandServices/CategoryCommandService.cs
+++ ACME.LearningCenterPlatform.API/Publishing/Application/Internal/CommandServices/CategoryCommandService.cs
@@ -1,12 +1,14 @@
 using ACME.LearningCenterPlatform.API.Publishing.Domain.Model.Commands;
 using ACME.LearningCenterPlatform.API.Publishing.Domain.Model.Entities;
+using ACME.LearningCenterPlatform.API.Publishing.Domain.Model.Events;
 using ACME.LearningCenterPlatform.API.Publishing.Domain.Repositories;
 using ACME.LearningCenterPlatform.API.Publishing.Domain.Services;
 using ACME.LearningCenterPlatform.API.Shared.Domain.Repositories;
+using Cortex.Mediator;
 
 namespace ACME.LearningCenterPlatform.API.Publishing.Application.Internal.CommandServices;
 
-public class CategoryCommandService(ICategoryRepository categoryRepository, IUnitOfWork unitOfWork) 
+public class CategoryCommandService(ICategoryRepository categoryRepository, IUnitOfWork unitOfWork, IMediator domainEventPublisher) 
     : ICategoryCommandService
 {
     public async Task<Category?> Handle(CreateCategoryCommand command)
@@ -14,6 +16,11 @@ public class CategoryCommandService(ICategoryRepository categoryRepository, IUni
         var category = new Category(command);
         await categoryRepository.AddAsync(category);
         await unitOfWork.CompleteAsync();
+        
+        // Publish the domain event after the category is created
+        await domainEventPublisher.PublishAsync(new CategoryCreatedEvent(category.Name));
+        
+        // Return the created category
         return category;
     }
 }
